f981d31002433f1735297190fa8820dae32c55e6
Joseph T. Lapp <arachnojoe@gmail.com>
2023-04-19 10:00:59 -0400

    feat!: remove query type exports for now, until I understand their use cases

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,4 @@ export * from './mappers/table-mapper-settings';
 export * from './mappers/table-mapper-transforms';
 export * from './mappers/uniform-table-mapper-settings';
 export * from './mappers/uniform-table-mapper';
-export * from './queries/delete-query';
-export * from './queries/insert-query';
-export * from './queries/select-query';
-export * from './queries/update-query';
 export * from './lib/query-filter';
