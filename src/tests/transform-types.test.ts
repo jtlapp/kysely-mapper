12d4b904637c4af5de5b9191214ecfd8865d3f03
Joseph T. Lapp <arachnojoe@gmail.com>
2023-04-19 21:38:04 -0400

    test: revise transform tests for withTransforms() and split return object

--- src/tests/transform-types.test.ts
+++ src/tests/transform-types.test.ts
@@ -1,8 +1,8 @@
-import { Insertable, Kysely, Selectable, Updateable } from 'kysely';
+import { Kysely } from 'kysely';
 
 import { TableMapper } from '../mappers/table-mapper';
 import { createDB, resetDB, destroyDB } from './utils/test-setup';
-import { Database, Users } from './utils/test-tables';
+import { Database } from './utils/test-tables';
 import { User } from './utils/test-types';
 import { ignore } from './utils/test-utils';
 import { createInsertTransformMapper } from './utils/test-mappers';
@@ -17,90 +17,45 @@ afterAll(() => destroyDB(db));
 
 describe('table mapper transform type checks', () => {
   ignore('detects invalid select transform configuration', () => {
-    class TestMapper extends TableMapper<Database, 'users', [], ['*'], User> {}
-    // @ts-expect-error - invalid select transform
-    new TestMapper(db, 'users', { selectTransform: (user) => user });
+    new TableMapper(db, 'users').withTransforms({
+      // @ts-expect-error - invalid select transform
+      selectTransform: (user: User) => user,
+    });
   });
 
   ignore('detects invalid insert transform configuration', () => {
-    class TestMapper extends TableMapper<
-      Database,
-      'users',
-      ['id'],
-      ['*'],
-      Selectable<Users>,
-      User
-    > {}
-    // @ts-expect-error - invalid insert transform
-    new TestMapper(db, 'users', { insertTransform: (user) => user });
-  });
-
-  ignore('detects invalid insert return transform configurations', () => {
-    class TestMapper extends TableMapper<
-      Database,
-      'users',
-      ['id'],
-      ['*'],
-      Selectable<Users>,
-      User,
-      Updateable<Users>,
-      bigint,
-      ['id']
-    > {}
-    new TestMapper(db, 'users', {
-      // @ts-expect-error - invalid insert return transform
-      insertReturnTransform: (_user) => ({ noId: 1 }),
-    });
-    new TestMapper(db, 'users', {
-      // @ts-expect-error - invalid insert return transform
-      insertReturnTransform: (user) => user,
+    new TableMapper(db, 'users').withTransforms({
+      // @ts-expect-error - invalid insert transform
+      insertTransform: (user: User) => user,
     });
   });
 
   ignore('detects invalid update transform configuration', () => {
-    class TestMapper extends TableMapper<
-      Database,
-      'users',
-      ['id'],
-      ['*'],
-      Selectable<Users>,
-      Insertable<Users>,
-      User,
-      bigint,
-      ['id']
-    > {}
-    new TestMapper(db, 'users', {
+    new TableMapper(db, 'users').withTransforms({
       // @ts-expect-error - invalid update transform
       updateTransform: (_user) => ({ noId: 1 }),
     });
   });
 
   ignore('detects invalid update return transform configurations', async () => {
-    class TestMapper<
+    function createTestMapper<
       UpdateReturnsSelectedObjectWhenProvided extends boolean
-    > extends TableMapper<
-      Database,
-      'users',
-      ['id'],
-      ['*'],
-      User,
-      Insertable<Users>,
-      User | Updateable<Users>,
-      bigint,
-      ['id'],
-      false,
-      UpdateReturnsSelectedObjectWhenProvided
-    > {}
-    new TestMapper<false>(db, 'users', {
+    >(
+      updateReturnsSelectedObjectWhenProvided: UpdateReturnsSelectedObjectWhenProvided
+    ) {
+      return new TableMapper(db, 'users', {
+        keyColumns: ['id'],
+        updateReturnsSelectedObjectWhenProvided,
+      });
+    }
+
+    createTestMapper(true).withTransforms({
+      selectTransform: (_user) => new User(1, 'John', 'Doe', 'jdoe', 'x@y.z'),
+      updateTransform: (user: User) => user,
       // @ts-expect-error - invalid update return transform
-      updateReturnTransform: (_user) => ({ noId: 1 }),
+      updateReturnTransform: (_user, _returns) => ({ noId: 1 }),
     });
-    new TestMapper<true>(db, 'users', {
-      // @ts-expect-error - invalid update return transform
-      updateReturnTransform: (_user) => ({ noId: 1 }),
-    });
-    const testMapper = new TestMapper<true>(db, 'users');
-    (await testMapper
+    (await createTestMapper(true)
       .update({ id: 1 })
       // @ts-expect-error - ensure that return type is User
       .returnOne(new User(1, 'John', 'Doe', 'jdoe', 'jdoe@abc.def')))!.name;
