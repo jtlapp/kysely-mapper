f8264af7ca7d0757d9b39e34e3fb737dc26a7869
Joseph T. Lapp <arachnojoe@gmail.com>
2023-04-22 14:43:31 -0400

    test: test use of TableMapper.ref()

--- src/tests/select-all.test.ts
+++ src/tests/select-all.test.ts
@@ -109,6 +109,24 @@ describe('selecting all returns', () => {
     expect(users.length).toEqual(0);
   });
 
+  it('selects with a binary operation filter using .ref()', async () => {
+    await userMapper.insert().run(USERS);
+
+    // Test selecting by condition (with results)
+    let users = await userMapper
+      .select(userMapper.ref('name'), '=', USERS[0].name)
+      .returnAll();
+    expect(users.length).toEqual(2);
+    expect(users[0].handle).toEqual(USERS[0].handle);
+    expect(users[1].handle).toEqual(USERS[2].handle);
+
+    // Test selecting by condition (no results)
+    users = await userMapper
+      .select(userMapper.ref('email'), '=', 'nonexistent')
+      .returnAll();
+    expect(users.length).toEqual(0);
+  });
+
   it('selects with a query expression filter', async () => {
     await userMapper.insert().run(USERS);
 
